0efc3201334c3e37037241f5faba8493f6f92d68
issa900 <diop.seydina@laposte.net>
2018-04-14 13:26:44 +0200

    Count read mapped in the output files of FeatureCount

--- featureC.py
+++ featureC.py
@@ -23,9 +23,10 @@ def read_count(input_file):
             Geneid=tmp[0]
             Chr=tmp[1]
             numFeature=len(Chr) # the number of read is considered here as the number of feauture detect on chromosome.
+            matchLen= tmp[5]
             chr_name=Chr.split(';')[0]
            
-            if chr_name in MYdict:
+            if chr_name in MYdict and int(matchLen)>=100:
                 MYdict[chr_name]+=numFeature
             else:
                 MYdict[chr_name]=numFeature
@@ -39,60 +40,63 @@ def read_count(input_file):
                 ChrY_count+=int(MYdict[key])
             else:
                 autosomal_count+=int(MYdict[key])
+        print(ChrY_count)
         #print('number of reads on Y Chr =', ChrY_count)
         #print('number of reads on autosomes =', autosomal_count)
         #print('Total of mapped reads=', ChrY_count + autosomal_count)
         
         return MYdict
 
-def match_X_scaffold (list_X_specificGene, gtf_file):
-    " Match genes on X chromosomes file found in the gtf file and extract the gene_id of the corresponding by storig it in the dict geneMatch "
-    with open (list_X_specificGene) as xGene, open (gtf_file) as gtf:
-        
-        next(xGene)
-        
-        geneXlist=[]
-        dico_gene={}
-        geneMatch={}
-        
-        for line in xGene:
-            line=line.strip()
-            if line:
-                gene_X=line.split()[0]
-                geneXlist.append(gene_X)
-        print(len('Number of X chr genes ='geneXlist))
-        
-        for gene in gtf:
-            tmp=gene.split()
-            gene_id=tmp[11].strip('";')
-            gene_name=tmp[13].strip('";')
-            
-            if gene_id in dico_gene:
-                dico_gene[gene_id].append(genename)
-            else:
-                dico_gene[gene_id]=gene_name
-            
-        for x in dico_gene:
-            val=dico_gene[x]
-            #for x in gene
-            if val in geneXlist:
-                print(val)
-                if x in geneMatch:
-                    geneMatch[x].append(val)
-                else:
-                    geneMatch[x]=val 
-        
-        return geneMatch
-                
-            
-                
 
     
-# input_file='/home/issa/Documents/prog/LCC.bam.txt'
+#def match_X_scaffold (list_X_specificGene, gtf_file):
+    " Match genes on X chromosomes file found in the gtf file and extract the gene_id of the corresponding by storig it in the dict geneMatch "
+    #with open (list_X_specificGene) as xGene, open (gtf_file) as gtf:
+        
+        #next(xGene)
+        
+        #geneXlist=[]
+        #dico_gene={}
+        #geneMatch={}
+        
+        #for line in xGene:
+            #line=line.strip()
+            #if line:
+                #gene_X=line.split()[0]
+                #geneXlist.append(gene_X)
+        #print('Number of X chr genes =', len(geneXlist)) #Number of Xgenes found on the genome paper
+        
+        #for gene in gtf:
+            #tmp=gene.split()
+            #gene_id=tmp[11].strip('";')
+            #gene_name=str(tmp[13].strip('";'))
+            
+            #if not gene_id in dico_gene.keys():
+                #dico_gene[gene_id]=gene_name
+            
+            #else:
+                #dico_gene[gene_id].append(genename)
+            
+        #for x in dico_gene:
+            #val=dico_gene[x]
+            ##for x in gene
+            #if val in geneXlist:
+                #print(val)
+                #if x in geneMatch:
+                    #geneMatch[x].append(val)
+                #else:
+                    #geneMatch[x]=val 
+        
+        #return geneMatch
+                
+
+#def match_xgene (f1, ID_Xgenes)         
+                
+
 count_file=sys.argv[1] # Output file of the program FeatureCount(.txt)
-gtf=sys.argv[2] # GTF file (Dowload the GFF3 file from Marchantia.info and convert into GTF by gffread pachage
-x=sys.argv[3] # List of genes identified on the X chromosome
+#gtf=sys.argv[2] # GTF file (Dowload the GFF3 file from Marchantia.info and convert into GTF by gffread pachage
+#x=sys.argv[3] # List of genes identified on the X chromosome
 
 f1=read_count(count_file)
 
-match_X_scaffold(x, gtf)
+#ID_Xgenes=match_X_scaffold(x, gtf)
